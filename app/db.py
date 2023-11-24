922ce0dee876e6e341e5f60a5f06ebe6094e2abb
Moez Mansouri <mmansouri@gopartner.fr>
2023-11-24 18:29:44 +0100

    feature/v1:add db.py

---
+++ app/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+import os
+
+# SQLAlchemy setup
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
